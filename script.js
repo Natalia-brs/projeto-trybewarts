54d927dad3d2ad2830bd3225b4b7d2098238e71e
natalia <ntla.brs@gmail.com>
2022-10-15 16:07:12 -0300

    Requisito 18 e 19

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const getInputEmail = document.getElementById('email-box');
 const getButton = document.getElementById('button-login');
+const getSubmitButton = document.getElementById('submit-btn');
+const agree = document.getElementById('agreement');
+
+function buttonPattern(e) {
+  e.preventDefault();
+}
+
+function checkAgree() {
+  getSubmitButton.disabled = !agree.checked;
+}
+
+window.onload = () => {
+  getSubmitButton.addEventListener('click', buttonPattern);
+  agree.addEventListener('change', checkAgree);
+};
 
 function loginAlert() {
   const dados = ['tryber@teste.com', 123456];
@@ -13,7 +28,5 @@ function loginAlert() {
     }
   });
 }
-
-//comentario
 
 loginAlert();
