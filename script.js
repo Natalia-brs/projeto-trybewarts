5cab780fe76dd24a590f6837aa27710f757b9c3d
leticiacanedo <leticiacanedo34@gmail.com>
2022-10-14 22:35:35 -0300

    o que ja tinhamos feito

---
+++ script.js
@@ -0,0 +1,18 @@
+java script 
+const getInputEmail = document.getElementById('email-box');
+const getButton = document.getElementById('button-login');
+
+function loginAlert() {
+  const dados = ['tryber@teste.com', 123456];
+  localStorage.setItem('user', JSON.stringify(dados));
+  const recoverUser = JSON.parse(localStorage.getItem('user'));
+  getButton.addEventListener('click', () => {
+    if (recoverUser[0] === getInputEmail.value && recoverUser[1] === 123456) {
+      alert('Olá, Tryber!');
+    } else {
+      alert('Email ou senha inválidos.');
+    }
+  });
+}
+
+loginAlert();
